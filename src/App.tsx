c4ffac67de3b8edffb1d41df18e572b1233d41b7
SitesProjects229 <netlyfuu@icloud.com>
2026-02-16 11:08:36 +0000

    asd

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
